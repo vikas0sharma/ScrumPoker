ef79c969859867353b0f018b4f0e35a463ba0fe3
Vikas Sharma <vikas.sharma@quovantis.com>
2020-05-23 15:17:11 +0530

    support for 13 point added

--- src/API/clientapp/src/components/admin/SprintDetail.tsx
+++ src/API/clientapp/src/components/admin/SprintDetail.tsx
@@ -8,85 +8,74 @@ import CopyText from '../common/CopyText';
 import { adminContext } from '../../models/context';
 
 export const SprintDetail = () => {
-  const [boardName, setBoardName] = useState('');
-  const [id, setUserLink] = useState('');
-  const [isLoading, setLoadingState] = useState(false);
-  const [linkCreated, setLinkState] = useState(false);
-  const [href, setHref] = useState('');
-  const { setAdmin } = useContext(adminContext);
-  const onSubmitHandler = async () => {
-    setLoadingState(true);
-    const id = await createBoard({
-      name: boardName,
-      description: '',
-    });
-    setLoadingState(false);
-    setLinkState(true);
-    setUserLink(id);
-    setHref(`${window.location.href}boards/${id}`);
+    const [boardName, setBoardName] = useState('');
+    const [isLoading, setLoadingState] = useState(false);
+    const { setAdmin } = useContext(adminContext);
+    const history = useHistory();
+    const onSubmitHandler = async () => {
+        setLoadingState(true);
+        
+        const id = await createBoard({
+            name: boardName,
+            description: '',
+        });
 
-    setAdmin(true);
-  };
-  const onCopyClick = () => {
-    CopyText(href);
-  };
+        setLoadingState(false);
+        setAdmin(true);
+        
+        history.push(`/boards/${id}`);
 
-  return (
-    <div className="container container-card">
-      <div className="card mb-3">
-        <div className="row no-gutters">
-          <King></King>
-          <div className="col-md-8">
-            <div className="card-body">
-              <h5 className="card-title">Create your poker board</h5>
-              <div className="form-group">
-                <label className="float-left">Sprint Name</label>
-                <input
-                  type="input"
-                  value={boardName}
-                  onChange={(e) => setBoardName(e.target.value)}
-                  className="form-control form-control-lg rounded-0"
-                />
-              </div>
-              <div className="form-group">
-                <label className="float-left">Description</label>
-                <input
-                  type="text"
-                  className="form-control form-control-lg rounded-0"
-                />
-              </div>
-              {linkCreated === true ? (
-                <div className="form-group">
-                  <label>Please visit and share the url with the team:</label>
-                  <Copy onClickHandler={onCopyClick}></Copy>
-                  <br />
-                  <Link to={`/boards/${id}`}>{href}</Link>
+    };
+
+
+    return (
+        <div className="container container-card">
+            <div className="card mb-3">
+                <div className="row no-gutters">
+                    <King></King>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title">Create your poker board</h5>
+                            <div className="form-group">
+                                <label className="float-left">Sprint Name</label>
+                                <input
+                                    type="input"
+                                    value={boardName}
+                                    onChange={(e) => setBoardName(e.target.value)}
+                                    className="form-control form-control-lg rounded-0"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label className="float-left">Description</label>
+                                <input
+                                    type="text"
+                                    className="form-control form-control-lg rounded-0"
+                                />
+                            </div>
+                            <button
+                                type="submit"
+                                onClick={() => onSubmitHandler()}
+                                className={`btn btn-default btn-lg float-left ${
+                                    isLoading ? 'disabled' : ''
+                                    }`}
+                            >
+                                {isLoading === true ? (
+                                    <>
+                                        <span
+                                            className="spinner-border spinner-border-sm mr-2"
+                                            role="status"
+                                            aria-hidden="true"
+                                        ></span>
+                                        <span>Creating ...</span>
+                                    </>
+                                ) : (
+                                        <span>Create</span>
+                                    )}
+                            </button>
+                        </div>
+                    </div>
                 </div>
-              ) : null}
-              <button
-                type="submit"
-                onClick={() => onSubmitHandler()}
-                className={`btn btn-default btn-lg float-left ${
-                  isLoading ? 'disabled' : ''
-                }`}
-              >
-                {isLoading === true ? (
-                  <>
-                    <span
-                      className="spinner-border spinner-border-sm mr-2"
-                      role="status"
-                      aria-hidden="true"
-                    ></span>
-                    <span>Creating ...</span>
-                  </>
-                ) : (
-                  <span>Create</span>
-                )}
-              </button>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
